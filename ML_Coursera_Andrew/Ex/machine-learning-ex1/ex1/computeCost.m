4bb95ac372a6feb93eafa18eeb9134b8d924fe63
jinghuaz <jhzhu@outlook.com>
2017-10-29 17:20:19 +0800

    Compute cost

--- ML_Coursera_Andrew/Ex/machine-learning-ex1/ex1/computeCost.m
+++ ML_Coursera_Andrew/Ex/machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+% varComputeCost1 = X * theta - y;
+J = 1 / (2 * m) * sum((X * theta - y).^2);
 
 % =========================================================================
 
